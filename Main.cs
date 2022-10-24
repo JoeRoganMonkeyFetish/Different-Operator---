ada501838cd027eed94d81a46518e91b0ce60f37
JoeRoganMonkeyDisease <114947306+JoeRoganMonkeyFetish@users.noreply.github.com>
2022-10-24 12:51:07 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,52 @@
+using System;
+  //Program to add two numbers; single line comment
+ class Program {
+    
+  
+      public static void Main (string[] args) {
+    int num1; //Declaration
+   int num2; //Declaration
+     int answer1; //Declaration
+      
+
+   int num3; //Declaration
+   int num4; //Declaration
+     int answer2; //Declaration
+
+      
+   int num5;  //Declaration
+   int num6; //Declaration
+     int answer3; //Declaration
+
+      
+   int num7; //Declaration
+    int num8; //Declaration
+     int answer4; //Declaration
+
+
+      
+    num1=20;//Intialization
+    num2=50;//Intialization
+    num3=60;//Intialization
+    num4=21;//Intialization
+    num5=5;//Intialization
+    num6=60;//Intialization
+    num7=4;//Intialization
+    num8=12;//Intialization
+
+
+      
+   answer1= num1+num2; 
+   answer2= num3-num4; 
+   answer3= num5/num6; 
+   answer4= num7*num8; 
+
+  
+  
+   Console.WriteLine ("answer="+answer1);
+   Console.WriteLine ("answer="+answer2);
+   Console.WriteLine ("answer="+answer3);
+   Console.WriteLine ("answer="+answer4);
+
+ }
+}
